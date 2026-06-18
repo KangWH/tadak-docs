9dc58c4cdf0f6e39b03e2626c4f3908b64127ab2
Woohyun Kang <kangwh.2003@gmail.com>
2026-06-19 08:27:28 +0900

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="max-w-240 mx-auto mt-4 mb-8 px-4 flex flex-col gap-y-2">
         <div className="flex flex-row gap-x-4">
           <strong>타닥</strong>
-          <Link href="/terms">이용 약관 및 면책 조항</Link>
+          <Link href="/terms">이용 약관</Link>
           <Link href="/privacy">개인정보 처리 방침</Link>
         </div>
         <p>Copyright 2026 강우현. All rights reserved.</p>
